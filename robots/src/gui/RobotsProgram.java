26992a2b0c99e056648566075819b1ae64a3d057
Fynoo <80120433+Broddanila@users.noreply.github.com>
2026-04-17 15:11:51 +0500

    exit-confirmation-and-localization
    Add exit confirmation dialog, menu refactoring, memory leak fixes and Russian localization for standard Swing components

---
+++ robots/src/gui/RobotsProgram.java
@@ -0,0 +1,23 @@
+package gui;
+
+import java.awt.Frame;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class RobotsProgram
+{
+    public static void main(String[] args) {
+      try {
+        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        RussianLocalization.setupFullRussianLocalization();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      SwingUtilities.invokeLater(() -> {
+        MainApplicationFrame frame = new MainApplicationFrame();
+        frame.pack();
+        frame.setVisible(true);
+        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+      });
+    }}
